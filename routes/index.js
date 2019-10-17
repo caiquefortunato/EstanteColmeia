bcf552c1970f24d4dc2c187293f06c219cebd859
Caíque Fortunato <caiquefortunato@ufmg.br>
2019-10-17 14:17:33 -0300

    CRUD + Upload de imagens

--- routes/index.js
+++ routes/index.js
@@ -27,10 +27,17 @@ router.post('/new', function(req, res) {
   var local = req.body.local;
   var serie = req.body.serie;
   
+  
   global.db.insert({titulo, autor, editora, paginas, genero, ano, local, serie}, (err, result) => {
-          if(err) { return console.log(err); }
-          res.redirect('/');
-  })
+    if(err) { return console.log(err); }
+  });
+  
+  global.db.findOneTitle(titulo, (e, docs) => {
+    if(e) { return console.log(e); }
+    console.log( docs);
+    res.redirect('/edit/'+docs[0]._id);
+  });
+
 })
 
 // ---------- Parte de edicao ---------- //
@@ -38,11 +45,12 @@ router.get('/edit/:id', function(req, res, next) {
   var id = req.params.id;
   global.db.findOne(id, (e, docs) => {
       if(e) { return console.log(e); }
-      res.render('new', { title: 'Edição de Cliente', doc: docs[0], action: '/edit/' + docs[0]._id });
+      res.render('new', { title: 'Edição de Livro', doc: docs[0], action: '/edit/' + docs[0]._id });
   });
 })
 
 router.post('/edit/:id', function(req, res) {
+
   var id = req.params.id;
   var titulo = req.body.titulo;
   var autor = req.body.autor;
@@ -52,11 +60,15 @@ router.post('/edit/:id', function(req, res) {
   var ano = parseInt(req.body.ano);
   var local = req.body.local;
   var serie = req.body.serie;
+  var idioma = req.body.idioma;
+  var isbn = req.body.isbn;
 
-  global.db.update(id, {titulo, autor, editora, paginas, genero, ano, local, serie}, (e, result) => {
+  global.db.update(id, {titulo, autor, editora, paginas, genero, ano, local, serie, idioma, isbn}, 
+    (e, result) => {
         if(e) { return console.log(e); }
         res.redirect('/');
-  });
+  })
+
 });
 
 // ---------- Parte de deleção ---------- //
@@ -80,3 +92,24 @@ router.get('/book/:id', function(req, res, next) {
       res.render('book', { title: 'Livro', doc: docs[0], action: '/book/' + docs[0]._id });
   });
 })
+
+router.post('/imagem/:id', function(req, res, next){
+  var formidable = require('formidable');
+  var fs = require('fs');
+  var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var oldpath = files.filetoupload.path;
+      var newpath = 'C:/EstanteColmeia/public/upload/'+req.params.id +'.jpg' ;
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.redirect('/edit/'+req.params.id);
+        //res.write('File uploaded and moved!');
+        //res.end();
+      });
+    });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Novo Cadastro', doc: {}, action: '/about' });
+});
+
